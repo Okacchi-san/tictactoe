77241c9af66d6593d4fbc4c485e911eeed574637
okacchi-san <okacchi1979@gmail.com>
2020-12-22 11:02:48 +0900

    add layout

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>Tic Toc Toe</h1>
+      <table id ="table">
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
   </div>
 </template>
 
@@ -12,5 +19,22 @@ export default{
 </script>
 
 <style scoped>
+  h1 {
+    text-align: center;
+  }
+  #table {
+    margin: 0 auto;
+    border-collapse: collapse;
+    border: 3px solid #ccc;
+  }
 
+  #table td {
+    border: 1px solid #ccc;
+    height: 80px;
+    width: 80px;
+    text-align: center;
+    vertical-align: middle;
+    font-size: 75px;
+    cursor: pointer;
+  }
 </style>
